9d11644aa1eee1a8f8ba2c56fb082d9ec4054c38
Admin <Admin@DESKTOP-HIJ6FM0>
2022-11-19 19:51:18 +0530

    This is for jenkins

---
+++ Parcas/src/test/java/jenkins/Tc2_Test.java
@@ -0,0 +1,20 @@
+package jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Tc2_Test {
+	@Test
+	
+	public void Tc()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://amazon.com");
+	}
+
+
+}
